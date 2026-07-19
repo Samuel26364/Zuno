26fb04369d4d62a5f8deb6557b824c5d59c65214
Samuel26364 <samuelbotty27@gmail.com>
2026-07-19 00:09:55 +0000

    Ajout config Supabase

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://qnxwufhhmmbpbxkrsyqx.supabase.co";
+const SUPABASE_KEY = "sb_publishable_3NWaFSMw7zej2934pqtMuw_6sriGax6";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
